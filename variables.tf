26d1fd4fb413eee8b096648298543223ff010dbc
Karsten Ari Agathon <karsten.agathon@traveloka.com>
2018-09-28 14:58:31 +0700

    Update CodeBuild docker image to v0.1.1 (#4)

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "compute_type" {
 
 variable "image" {
   type        = "string"
-  default     = "traveloka/codebuild-terraform-ci-cd-image:v0.1.0"
+  default     = "traveloka/codebuild-terraform-ci-cd-image:v0.1.1"
   description = "Docker image used by CodeBuild"
 }
 

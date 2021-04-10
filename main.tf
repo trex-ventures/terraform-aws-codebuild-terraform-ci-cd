1afe1ca6cbc3b15b046a2ac7a903108b521cf4ef
Ikhsan Noor Rosyidin <ikhsan.rosyidin@traveloka.com>
2021-04-10 12:41:43 +0700

    [CTD-670] Block public access of S3 bucket for codebuild artifact (#28)

--- main.tf
+++ main.tf
@@ -115,6 +115,15 @@ resource "aws_s3_bucket" "artifact" {
   )}"
 }
 
+resource "aws_s3_bucket_public_access_block" "block_public_access" {
+  bucket = "${aws_s3_bucket.artifact.id}"
+
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
+}
+
 ######
 # CI #
 ######
